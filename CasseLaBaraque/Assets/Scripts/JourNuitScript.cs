12d8d3e40f379a7397f12df242813b385c12acf8
msaouter <msaouter@gmail.com>
2019-10-19 18:49:01 +0200

    Neighbour script add

---
+++ CasseLaBaraque/Assets/Scripts/JourNuitScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JourNuitScript : MonoBehaviour
+{
+
+    public Color nuit;
+    public Color jour;
+
+    public DifficultySettings settings;
+
+    private Coroutine fadeJourNuitCoroutine;
+
+
+    public void Start()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        fadeJourNuitCoroutine = StartCoroutine(fadeJourNuit());
+    }
+
+
+    private IEnumerator fadeJourNuit()
+    {
+        Camera.main.backgroundColor = nuit;
+        float timer = 0;
+
+        while(timer < settings.playTimeInSeconds)
+        {
+            float ratio = timer / settings.playTimeInSeconds;
+
+            Color newColor = Color.Lerp(nuit, jour, ratio);
+
+            timer += Time.deltaTime;
+
+            Camera.main.backgroundColor = newColor;
+
+            yield return null;
+        }
+        
+    }
+
+    public void EndGame()
+    {
+        if(fadeJourNuitCoroutine != null)
+        {
+            StopCoroutine(fadeJourNuitCoroutine);
+            fadeJourNuitCoroutine = null;
+        }
+    }
+
+}
---
+++ CasseLaBaraque/Assets/Scripts/Shake.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+ 
+public class Shake : MonoBehaviour
+{
+    private Vector3 startPosition;
+
+
+    public float magnitudeMultiplicator = 1;
+    public float magnitudeDbMultiplicator = 1;
+    public float waitingBaseTime = 0;
+    public float waitingTimeMultiplicator = 1;
+    public float yVariation = 1;
+    public float xVariation = 1;
+
+    float magnitude;
+    float waitingTime;
+    float timeWaited;
+
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        timeWaited = 0;
+    }
+
+
+
+    public void SetMagnitude(float microDb)
+    {
+
+
+        if (microDb <= GameManager.Instance.getDbCalme())
+            magnitude = 0;
+        else
+            this.magnitude = microDb;
+    }
+
+    public void SetWaitingTime(float microDb)
+    {
+        waitingTime = waitingBaseTime * waitingTimeMultiplicator * 1 / microDb;
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        float microDb = GameManager.Instance.getDbMicro();
+        SetWaitingTime(microDb);
+
+        if (waitingTime < timeWaited)
+        {
+            SetMagnitude(microDb);
+            transform.position = magnitude * magnitudeDbMultiplicator * magnitudeMultiplicator * new Vector3(Random.Range(-xVariation, xVariation), Random.Range(-yVariation, yVariation), 0) + startPosition;
+            timeWaited = 0;
+        }
+        else
+            timeWaited += Time.deltaTime;
+    }
+
+
+}
